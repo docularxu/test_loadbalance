e148e2a53be88fbe9e01c9462b4e5ca4e960dd29
Guodong Xu <guodong.xu@linaro.org>
2022-11-14 17:36:17 +0800

    Initial commit
    Verified with loading providers from a config file,
    and verified loading from source code.
    
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

---
+++ README.md
@@ -0,0 +1,10 @@
+# test_loadbalance
+
+1. To compile, run these:
+
+mkdir build
+cmake -S . -B build --log-level=DEBUG
+cmake --build build --verbose
+
+2. To launch the test, run:
+./build/test_lbprov
---
+++ test_lb.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <openssl/provider.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+
+// #define COMPARE_WITH_DEFAULT 1
+#define PRINT_PREFIX "****** "
+
+/*
+ * return:
+ *   1, failed
+ *   0, success
+ */
+static int calc_md5(OSSL_LIB_CTX *libctx, char *prop)
+{
+    EVP_MD_CTX *ctx = NULL;
+    EVP_MD *md5 = NULL;
+    const unsigned char msg[] = {
+        0x00, 0x01, 0x02, 0x03
+    };
+    unsigned int len = 0;
+    unsigned char *outdigest = NULL;
+    int ret = 1;
+
+    md5 = EVP_MD_fetch(libctx, "MD5", prop);
+    if (md5 == NULL) {
+        printf("Failed to fetch MD5\n");
+        goto err;
+    }
+
+    printf(PRINT_PREFIX "Success EVP_MD_fetch MD5 from libctx\n");
+
+    /* Use the digests */
+
+    /* Create a context for the digest operation */
+    ctx = EVP_MD_CTX_new();
+    if (ctx == NULL) {
+        printf(PRINT_PREFIX "Err 1\n");
+        goto err;
+    }
+
+    /* Initialise the digest operation */
+    if (!EVP_DigestInit_ex(ctx, md5, NULL)) {
+        printf(PRINT_PREFIX "Err 2\n");
+        goto err;
+    }
+
+    /*
+     * Pass the message to be digested. This can be passed in over multiple
+     * EVP_DigestUpdate calls if necessary
+     */
+    if (!EVP_DigestUpdate(ctx, msg, sizeof(msg))) {
+        printf(PRINT_PREFIX "Err 3\n");
+        goto err;
+    }
+
+    /* Allocate the output buffer */
+    printf(PRINT_PREFIX "EVP_MD_get_size()=%d\n", EVP_MD_get_size(EVP_MD_CTX_get0_md(ctx)));
+    outdigest = OPENSSL_malloc(EVP_MD_get_size(EVP_MD_CTX_get0_md(ctx)));
+    /* NOTE: cannot use md5, because the real method is fetched is modified by EVP_DigestInit_ex(ctx) */
+    if (outdigest == NULL) {
+        printf(PRINT_PREFIX "Err 4\n");
+        goto err;
+    }
+
+    /* Now calculate the digest itself */
+    if (!EVP_DigestFinal_ex(ctx, outdigest, &len)) {
+        printf(PRINT_PREFIX "Err 5\n");
+        goto err;
+    }
+
+    /* Print out the digest result */
+    BIO_dump_fp(stdout, outdigest, len);
+
+    ret = 0;
+
+err:
+    /* Clean up all the resources we allocated */
+    OPENSSL_free(outdigest);
+    EVP_MD_free(md5);
+    EVP_MD_CTX_free(ctx);
+    if (ret != 0)
+       ERR_print_errors_fp(stderr);
+    return ret;
+}
+
+int main(void)
+{
+    OSSL_LIB_CTX *lb_libctx = NULL;
+    OSSL_PROVIDER *lbprov = NULL;
+    OSSL_PROVIDER *deflt = NULL;
+    int ret = 1;
+
+#ifdef COMPARE_WITH_DEFAULT
+    OSSL_PROVIDER *deflt2;
+    deflt2 = OSSL_PROVIDER_load(NULL, "default");
+    if (deflt2 == NULL) {
+        printf(PRINT_PREFIX "Failed to load Default provider\n");
+        goto err;
+    }
+
+    if (calc_md5(NULL, NULL) != 0) {
+        printf(PRINT_PREFIX "Failed to calculate MD5 using the default provider\n");
+        goto err;
+    }
+#endif
+
+    /*
+     * Create load-balancing library contexts
+     */
+    lb_libctx = OSSL_LIB_CTX_new();
+    if (lb_libctx == NULL)
+        goto err;
+
+#if 1   /* load from config file */
+    /*
+     * Load config file for the load-balancing library context. We assume that
+     * this config file will automatically activate the load-balancing
+     * provider and the default provider.
+     */
+    if (!OSSL_LIB_CTX_load_config(lb_libctx, "openssl-loadbalancing.cnf"))
+        goto err;
+    printf(PRINT_PREFIX \
+           "Succeeded to load loadbalance and default providers by config file\n");
+#else    /* load explicitly */
+    lbprov = OSSL_PROVIDER_load(lb_libctx, "loadbalance");
+    if (lbprov == NULL) {
+        printf(PRINT_PREFIX "Failed to load loadbalance provider\n");
+        goto err;
+    }
+    printf(PRINT_PREFIX "Succeeded to load loadbalance provider, %p\n", (void *)lbprov);
+
+    deflt = OSSL_PROVIDER_load(lb_libctx, "default");
+    if (deflt == NULL) {
+        printf(PRINT_PREFIX "Failed to load Default provider into lb_libctx\n");
+        goto err;
+    }
+    printf(PRINT_PREFIX "Succeeded to load default provider, %p\n", (void *)deflt);
+#endif
+
+    /* As an example get some digests */
+
+    /*
+     * Calculate MD5 for doing the digest. We're
+     * using the "lb_libctx" library context here.
+     */
+    if (calc_md5(lb_libctx, "provider=loadbalance") != 0) {
+        printf(PRINT_PREFIX "Failed to calculate MD5 using the lb_libctx provider\n");
+        goto err;
+    }
+    ret = 0;
+
+err:
+    /* Clean up all the resources we allocated */
+    OSSL_LIB_CTX_free(lb_libctx);
+#ifdef COMPARE_WITH_DEFAULT
+    OSSL_PROVIDER_unload(deflt2);
+#endif
+    OSSL_PROVIDER_unload(lbprov);
+    OSSL_PROVIDER_unload(deflt);
+    if (ret != 0)
+       ERR_print_errors_fp(stderr);
+    return ret;
+}
+
+
