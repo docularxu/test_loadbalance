f2128d142b9069d868c276e76c7b01dfc6af2417
Guodong Xu <guodong.xu@linaro.org>
2022-12-17 11:40:31 +0800

    Add digest life cycle tests
    Test EVP_MD_CTX life cycles, it covered three type of transitions:
    1. EVP_MD_CTX_reset() -> DigestInit
    2. DigestUpdate -> DigestInit
    3. DigestFinal -> DigestInit
    
    Refering to
    https://www.openssl.org/docs/man3.0/man7/life_cycle-digest.html
    
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- test_lb.c
+++ test_lb.c
@@ -23,6 +23,98 @@
 #define PRINT_PREFIX "****** "
 #define COUNT_REPETITION (10*1)
 
+#define COUNT_CTX_RESET_LOOPS (10000)     /* inner loop counts of ctx reset test*/
+/* calc_md5_ctx_reset - test MD_CTX cycle with EVP_MD_CTX_reset
+ * return:
+ *   1, failed
+ *   0, success
+ */
+static int calc_md5_ctx_reset(OSSL_LIB_CTX *libctx, char *prop, int loop)
+{
+    EVP_MD_CTX *ctx = NULL;
+    EVP_MD *md5 = NULL;
+    const unsigned char msg[] = {
+        0x00, 0x01, 0x02, 0x03
+    };
+    unsigned int len = 0;
+    unsigned char *outdigest = NULL;
+    int ret = 1;
+
+    md5 = EVP_MD_fetch(libctx, "MD5", prop);
+    if (md5 == NULL) {
+        printf("Failed to fetch MD5\n");
+        goto err;
+    }
+    printf(PRINT_PREFIX "Success EVP_MD_fetch MD5 from libctx\n");
+
+    /* Use the digests */
+
+    /* Create a context for the digest operation */
+    ctx = EVP_MD_CTX_new();
+    if (ctx == NULL) {
+        printf(PRINT_PREFIX "Err 1\n");
+        goto err;
+    }
+    for (int i = 0; i < COUNT_CTX_RESET_LOOPS; i++) {
+        printf(PRINT_PREFIX "Round %d, EVP_MD_CTX Reset Round %d:\n", loop, i);
+
+        /* Initialise the digest operation */
+        if (!EVP_DigestInit_ex(ctx, md5, NULL)) {
+            printf(PRINT_PREFIX "Err 2\n");
+            goto err;
+        }
+
+        /* print load-balancer result */
+        printf(PRINT_PREFIX "Using actual implementation, name: %s, description: %s\n",
+                EVP_MD_get0_name(EVP_MD_CTX_get0_md(ctx)),
+                EVP_MD_get0_description(EVP_MD_CTX_get0_md(ctx)));
+        printf(PRINT_PREFIX "EVP_MD_get_size()=%d\n", EVP_MD_get_size(EVP_MD_CTX_get0_md(ctx)));
+
+        if (!EVP_DigestUpdate(ctx, msg, sizeof(msg))) {
+            printf(PRINT_PREFIX "Err 3\n");
+            goto err;
+        }
+
+        if ((i % 3) == 0) {
+            printf(PRINT_PREFIX PRINT_PREFIX "Transit from DigestUpdate -> DigestInit\n");
+            continue;
+        }
+
+        /* Allocate the output buffer */
+        outdigest = OPENSSL_malloc(EVP_MD_get_size(EVP_MD_CTX_get0_md(ctx)));
+        if (outdigest == NULL) {
+            printf(PRINT_PREFIX "Err 4\n");
+            goto err;
+        }
+
+        if (!EVP_DigestFinal_ex(ctx, outdigest, &len)) {
+            printf(PRINT_PREFIX "Err 5\n");
+            goto err;
+        }
+
+        /* Print out the digest result */
+        BIO_dump_fp(stdout, outdigest, len);
+        OPENSSL_free(outdigest);
+
+        if ((i % 3) == 1) {
+            printf(PRINT_PREFIX PRINT_PREFIX "Transit from DigestFinal -> DigestInit\n");
+            continue;
+        }
+
+        EVP_MD_CTX_reset(ctx);
+    } /* end of for */
+
+    ret = 0;
+
+err:
+    /* Clean up all the resources we allocated */
+    EVP_MD_CTX_free(ctx);
+    EVP_MD_free(md5);
+    if (ret != 0)
+       ERR_print_errors_fp(stderr);
+    return ret;
+}
+
 /*
  * return:
  *   1, failed
@@ -186,7 +278,11 @@ int main(void)
      */
     for (int i = 0; i  < COUNT_REPETITION; i++) {
         printf(PRINT_PREFIX "Round %d\n", i);
+#if 0
         if (calc_md5(parent_libctx, "provider=loadbalance") != 0) {
+#else
+        if (calc_md5_ctx_reset(parent_libctx, "provider=loadbalance", i) != 0) {
+#endif
             printf(PRINT_PREFIX "Round %d: Failed to calculate MD5 using the parent_libctx provider\n", i);
             goto err;
         }
